57cf2fda32bdb3bff1a4d18086d0fd98351e6451
mohamadat <31732297+mohamadat@users.noreply.github.com>
2018-12-19 16:53:58 +0100

    empty main

---
+++ src/Domain/trainPart/TestClass.java
@@ -0,0 +1,19 @@
+package Domain.trainPart;
+
+import DataSource.DataHandler;
+import DataSource.FileHandler;
+
+public class TestClass {
+/*
+	public static void main(String[] args) {
+		DataHandler dh = new FileHandler();
+				System.out.println(dh.selectAllTypes());
+				dh.addRollerType("Locomotive", 20);
+				dh.addRollerType("passenger_Firstclass", 25);
+				dh.addRollerType("passenger_Secoundclass", 25);
+
+	}
+
+*/
+
+}
